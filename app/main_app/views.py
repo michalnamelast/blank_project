8171e4666b95bc4eebccc0c46297e4f059d115a5
michal <michal.jaronczyk@ikonka.eu>
2023-09-12 15:52:53 +0200

    works

--- app/main_app/views.py
+++ app/main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django import forms
+
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label="Your name", max_length=100)
+    you_age = forms.CharField(label="Your age", max_length=100)
+    you_cash = forms.CharField(label="Your cash", max_length=100)
 
 
 def first_view(request):
-    return render(request, "first_template.html")
+    return render(request, "first_template.html", {'form': NameForm})
